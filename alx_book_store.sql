b849ee77c65318ee1d7d0d458ff4d499c4a2e439
Victor Mntungwa <147992270+za-mntungwa@users.noreply.github.com>
2025-07-06 23:56:08 +0200

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -4,7 +4,7 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 
 CREATE TABLE IF NOT EXISTS
     Books (
-        book_id PRIMARY KEY,
+        book_id INT PRIMARY KEY,
         title VARCHAR(130) NOT NULL,
         author_id VARCHAR(255) NOT NULL,
         price DOUBLE NOT NULL,
@@ -13,13 +13,13 @@ CREATE TABLE IF NOT EXISTS
 
 CREATE TABLE IF NOT EXISTS
     Authors (
-        author_id PRIMARY KEY,
+        author_id INT PRIMARY KEY,
         author_name VARCHAR(215) NOT NULL
     );
 
 CREATE TABLE IF NOT EXISTS
     Customers (
-        customer_id PRIMARY KEY,
+        customer_id INT PRIMARY KEY,
         customer_name VARCHAR(215) NOT NULL,
         email VARCHAR(215) NOT NULL,
         address TEXT NOT NULL
@@ -27,15 +27,18 @@ CREATE TABLE IF NOT EXISTS
 
 CREATE TABLE IF NOT EXISTS
     Orders (
-        order_id PRIMARY KEY,
-        customer_id NOT NULL,
-        order_date DATE NOT NULL
+        order_id INT PRIMARY KEY,
+        customer_id INT NOT NULL,
+        order_date DATE NOT NULL,
+        FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
     );
 
 CREATE TABLE IF NOT EXISTS
     Orders_Details (
-        orderdetailid PRIMARY KEY,
-        order_id ,
-        book_id NOT NULL,
-        quantity DOUBLE NOT NULL
+        orderdetailid INT PRIMARY KEY,
+        order_id INT NOT NULL,
+        book_id INT NOT NULL,
+        quantity DOUBLE NOT NULL,
+        FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+        FOREIGN KEY (book_id) REFERENCES Books(book_id)
     );
